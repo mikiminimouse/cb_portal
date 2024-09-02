19e3589df59a9beaaa257a8c729d93fcd1e969af
mikiminimouse <skullbv@gmail.com>
2024-09-03 03:51:13 +0700

    before set up filters

--- cb_portal/settings.py
+++ cb_portal/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bike_rental',
+    'import_export',
+    'django_filters',
+    # 'smart_selects',
 ]
 
 MIDDLEWARE = [
